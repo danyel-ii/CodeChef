e1527a5182f9f8ec60c3fdbde7a0a716a9a4dead
danyel-ii <222602034+danyel-ii@users.noreply.github.com>
2026-04-06 00:14:25 +0200

    Export Android reports to Downloads

--- apps/mobile/lib/src/features/workbench/report_exporter_io.dart
+++ apps/mobile/lib/src/features/workbench/report_exporter_io.dart
@@ -6,7 +6,12 @@ Future<String> exportMarkdownReport({
   required String fileName,
   required String markdown,
 }) async {
-  final Directory directory = await getApplicationDocumentsDirectory();
+  final Directory directory;
+  if (Platform.isAndroid) {
+    directory = await getDownloadsDirectory() ?? await getApplicationDocumentsDirectory();
+  } else {
+    directory = await getApplicationDocumentsDirectory();
+  }
   final exportDirectory = Directory('${directory.path}/exports');
   await exportDirectory.create(recursive: true);
   final file = File('${exportDirectory.path}/$fileName');
