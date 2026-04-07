15da153b52b8b1a47da9e1b351d4278e3f90764f
danyel-ii <222602034+danyel-ii@users.noreply.github.com>
2026-04-07 09:40:09 +0200

    Initialize SQLite before app startup

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile_recipe_lab_mobile/src/app/mobile_recipe_lab_app.dart';
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
   runApp(const ProviderScope(child: MobileRecipeLabApp()));
 }
